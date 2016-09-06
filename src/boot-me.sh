8f69ab620470b2a8e96b00b42450da0031fcbb9d
sophia.hudson <sophia.hudson@technicallyspeaking.io>
2016-09-06 17:24:42 -0600

    Moved bashprofile to profile

--- src/boot-me.sh
+++ src/boot-me.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/
 
 # Executes project-level environmental configuration and boot sequence for user
 
@@ -7,14 +7,14 @@ HEADER=INFO
 
 echo "Installing $PROJECT_NAME development environment for Google Cloud Platform"
 
-echo "  Installing component: bashprofile"
-BASHPROFILE_SRC=https://raw.githubusercontent.com/technicallyspeaking/debian.env.ops.technicallyspeaking.io/master/src/bashprofile
-BASHPROFILE_TARGET=~/.bashprofile
-echo "    - Source Location: $BASHPROFILE_SRC"
-echo "    - Target Location: $BASHPROFILE_TARGET"
-wget -q -O - $BASHPROFILE_SRC > $BASHPROFILE_TARGET
+echo "  Installing component: profile"
+PROFILE_SRC=https://raw.githubusercontent.com/technicallyspeaking/debian.env.ops.technicallyspeaking.io/master/src/profile
+PROFILE_TARGET=~/.profile
+echo "    - Source Location: $PROFILE_SRC"
+echo "    - Target Location: $PROFILE_TARGET"
+wget -q -O - $PROFILE_SRC > $PROFILE_TARGET
 echo "    $HEADER: Source downloaded and written to target location"
-source $BASHPROFILE_TARGET
+source $PROFILE_TARGET
 echo "    $HEADER: Component installation complete" 
 
 echo "Finished installing $PROJECT_NAME development environment for Google Cloud Platform"
