c2210349d84f900c023e719a8ea956edfdbecd6a
sophia.hudson <sophia.hudson@technicallyspeaking.io>
2016-09-06 17:52:57 -0600

    Going to do this as an init script

--- src/boot-me.sh
+++ src/boot-me.sh
@@ -7,14 +7,14 @@ HEADER=INFO
 
 echo "Installing $PROJECT_NAME development environment for Google Cloud Platform"
 
-echo "  Installing component: profile"
-PROFILE_SRC=https://raw.githubusercontent.com/technicallyspeaking/debian.env.ops.technicallyspeaking.io/master/src/profile
-PROFILE_TARGET=~/.profile
-echo "    - Source Location: $PROFILE_SRC"
-echo "    - Target Location: $PROFILE_TARGET"
-wget -q -O - $PROFILE_SRC > $PROFILE_TARGET
+echo "  Installing component: technicallyspeaking.init"
+INIT_SRC=https://raw.githubusercontent.com/technicallyspeaking/debian.env.ops.technicallyspeaking.io/master/src/technicallyspeaking.init
+INIT_TARGET=/etc/init.d/technicallyspeaking.init
+echo "    - Source Location: $INIT_SRC"
+echo "    - Target Location: $INIT_TARGET"
+sudo wget -q -O - $INIT_SRC > $INIT_TARGET
 echo "    $HEADER: Source downloaded and written to target location"
-source $PROFILE_TARGET
+source $INIT_TARGET
 echo "    $HEADER: Component installation complete" 
 
 echo "Finished installing $PROJECT_NAME development environment for Google Cloud Platform"
